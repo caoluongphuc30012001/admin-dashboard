4ff95651921edbeb516d2b8ef90b9c9ecccad0b5
phuccao.30012001@gmail.com <phuccao.30012001@gmail.com>
2022-10-07 02:53:30 +0700

    fix: url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: false,
     swcMinify: true,
     env: {
-        BACK_END_URL: 'http://localhost:3000'
+        BACK_END_URL: 'http://ec2-13-250-111-36.ap-southeast-1.compute.amazonaws.com:3000'
     }
 };
 
